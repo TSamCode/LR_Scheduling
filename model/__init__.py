af6a510ca3c8faa1cfdcb84e077988b00d38893d
TSamCode <69574160+TSamCode@users.noreply.github.com>
2021-06-23 14:09:52 +0100

    get_branch_params functionality added

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,4 @@
 from model.cnn_blocks import BasicBlock, Bottleneck, conv1x1, conv3x3
 from model.resnet import ResNet
 from model.resnet_branched import ResNetSplit, ResNetSplit18
+from model.processing import get_branch_params
