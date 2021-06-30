00c8f6a10766e3c85e9a6e9eecc9265cee78bab9
TSamCode <69574160+TSamCode@users.noreply.github.com>
2021-06-30 10:57:39 +0100

    creation of parallel ResNet with shared layers
    The two branches of this ResNet model only branch before the last two 'blocks' are added, all previous layers are shared

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
 from model.cnn_blocks import BasicBlock, Bottleneck, conv1x1, conv3x3
 from model.resnet import ResNet
 from model.resnet_branched import ResNetSplit, ResNetSplit18
+from model.resnet_branched_sharedLayers import ResNetSplitShared, ResNetSplit18Shared
 from model.processing import get_branch_params
