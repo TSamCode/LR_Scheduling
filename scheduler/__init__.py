3e428743461f936273db7543a54e99f62ede1fac
TSamCode <tom@adamsfamily.eu>
2021-09-06 11:45:13 +0100

    add cosine condition for 10 class scheduler

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -1,1 +1,1 @@
-from scheduler.congestion_avoidance import congestion_avoid, linear_cong_condition, congestion_avoid_10classes
+from scheduler.congestion_avoidance import congestion_avoid, linear_cong_condition, congestion_avoid_10classes, congestion_avoid_10classes_cosine
--- scheduler/congestion_avoidance.py
+++ scheduler/congestion_avoidance.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import torch.nn.functional as F
 
 
 def linear_cong_condition(min_cond, max_cond, epoch, max_epochs):
@@ -54,38 +55,29 @@ def congestion_avoid_10classes(cls_num, model, optimizer, metrics, condition, gr
     # Create a threshold which is the average metric from a number of the worst performing classes
     threshold = np.average(np.sort(metrics)[:num_class_avg])
 
-    total_grad = {'layer1': {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 'total':0},
-                  'layer2': {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 'total':0},
-                  'layer3': {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 'total':0},
-                  'layer4': {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 'total':0},
-                  'conv1': {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 'total':0},
-                  'bn1': {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 'total':0},
-                  'fc': {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 'total':0}
-                  }
-
     for cls in range(cls_num):
-        for name, value in model.named_parameters():
-            if 'layer1' in str(name):
-                total_grad['layer1'][cls] += float(abs(grads[cls][name]).sum())
-                total_grad['layer1']['total'] += float(abs(grads[cls][name]).sum())
-            elif 'layer2' in str(name):
-                total_grad['layer2'][cls] += float(abs(grads[cls][name]).sum())
-                total_grad['layer2']['total'] += float(abs(grads[cls][name]).sum())
-            elif 'layer3' in str(name):
-                total_grad['layer3'][cls] += float(abs(grads[cls][name]).sum())
-                total_grad['layer3']['total'] += float(abs(grads[cls][name]).sum())
-            elif 'layer4' in str(name):
-                total_grad['layer4'][cls] += float(abs(grads[cls][name]).sum())
-                total_grad['layer4']['total'] += float(abs(grads[cls][name]).sum())
-            elif 'conv1' in str(name):
-                total_grad['conv1'][cls] += float(abs(grads[cls][name]).sum())
-                total_grad['conv1']['total'] += float(abs(grads[cls][name]).sum())
-            elif 'bn1' in str(name):
-                total_grad['bn1'][cls] += float(abs(grads[cls][name]).sum())
-                total_grad['bn1']['total'] += float(abs(grads[cls][name]).sum())
-            elif 'fc' in str(name):
-                total_grad['fc'][cls] += float(abs(grads[cls][name]).sum())
-                total_grad['fc']['total'] += float(abs(grads[cls][name]).sum())
+        metric = metrics[cls]
+        if (threshold < condition * metric) and (epoch_counts[cls] >= min_epochs):
+            boolean_values[cls] = True
+            print('Condition has been met (class {}) ..... {:.2f}% --> {:.2f}%'.format(cls,100.*threshold, 100.*metric))
+            for name, value in model.named_parameters():
+                with torch.no_grad():
+                    if name in grads[cls].keys() and (('layer4' in str(name)) or ('fc' in str(name))):
+                            value += mult * grads[cls][name]
+                            grads[cls][name] -= mult * grads[cls][name]
+                            print('Gradient used: ', name)
+            epoch_counts[cls] = 0
+    
+        else:
+            print('Condition not met (class {}) ..... {:.2f}% --> {:.2f}%'.format(cls,100.*threshold, 100.*metric))
+
+    return optimizer, model, boolean_values, epoch_counts, grads
+
+
+def congestion_avoid_10classes_cosine(cls_num, model, optimizer, metrics, condition, grads, min_epochs, mult, epoch_counts, boolean_values, num_class_avg, similarity_threshold):
+
+    # Create a threshold which is the average metric from a number of the worst performing classes
+    threshold = np.average(np.sort(metrics)[:num_class_avg])
 
     for cls in range(cls_num):
         metric = metrics[cls]
@@ -96,32 +88,15 @@ def congestion_avoid_10classes(cls_num, model, optimizer, metrics, condition, gr
             boolean_values[cls] = True
             print('Condition has been met (class {}) ..... {:.2f}% --> {:.2f}%'.format(cls,100.*threshold, 100.*metric))
             for name, value in model.named_parameters():
+                cosine_sims = [float(abs(F.cosine_similarity(grads[cls][name], grads[index][name], dim=-1)).mean()) for index in range(cls_num)]
                 with torch.no_grad():
-                  # ADDED CONDITION TO ONLY CHANGE GRADIENTS FROM LAYER 3 ONWARDS
-                    if name in grads[cls].keys():# and (('layer4' in str(name)) or ('fc' in str(name))):
-                        if ('layer1' in str(name)) and (total_grad['layer1'][cls] < min_gradient * total_grad['layer1']['total']):
-                            value += mult * grads[cls][name]
-                            grads[cls][name] -= mult * grads[cls][name]
-                        elif ('layer2' in str(name)) and (total_grad['layer2'][cls] < min_gradient * total_grad['layer2']['total']):
-                            value += mult * grads[cls][name]
-                            grads[cls][name] -= mult * grads[cls][name]
-                        elif ('layer3' in str(name)) and (total_grad['layer3'][cls] < min_gradient * total_grad['layer3']['total']):
-                            value += mult * grads[cls][name]
-                            grads[cls][name] -= mult * grads[cls][name]
-                        elif ('layer4' in str(name)) and (total_grad['layer4'][cls] < min_gradient * total_grad['layer4']['total']):
-                            value += mult * grads[cls][name]
-                            grads[cls][name] -= mult * grads[cls][name]
-                        elif ('conv1' in str(name)) and (not any(layer in str(name) for layer in ('layer1','layer2','layer3','layer4'))) and (total_grad['conv1'][cls] < min_gradient * total_grad['conv1']['total']):
-                            value += mult * grads[cls][name]
-                            grads[cls][name] -= mult * grads[cls][name]
-                        elif ('bn1' in str(name)) and (not any(layer in str(name) for layer in ('layer1','layer2','layer3','layer4'))) and (total_grad['bn1'][cls] < min_gradient * total_grad['bn1']['total']):
-                            value += mult * grads[cls][name]
-                            grads[cls][name] -= mult * grads[cls][name]
-                        elif ('fc' in str(name)) and (not any(layer in str(name) for layer in ('layer1','layer2','layer3','layer4'))) and (total_grad['fc'][cls] < min_gradient * total_grad['fc']['total']):
-                            value += mult * grads[cls][name]
-                            grads[cls][name] -= mult * grads[cls][name]
+                    if name in grads[cls].keys():
+                        if min(cosine_sims) < similarity_threshold:
+                              value += mult * grads[cls][name]
+                              grads[cls][name] -= mult * grads[cls][name]
+                              print('Gradient used: ', name, ', Cosine sim: ', min(cosine_sims))
                         else:
-                            print('Gradient not used: ', name)
+                            print('Cosine similarity too high: ', name, '--> ', min(cosine_sims))
                             pass
             epoch_counts[cls] = 0
     
